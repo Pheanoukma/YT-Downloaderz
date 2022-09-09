459e50061e60edc9186bb28bab4c39aed6576c52
Pheanoukma <103352870+Pheanoukma@users.noreply.github.com>
2022-09-09 12:50:36 +0700

    Update yt_dlder.py

--- yt_dlder.py
+++ yt_dlder.py
@@ -1,45 +1,18 @@
-from pytube import youtube
+from pytube import YouTube
 
-banner = """
+#ask for the link from user
+link = input("Enter the link of YouTube video you want to download:  ")
+yt = YouTube(link)
 
-    Youtube video downloader 
+#Showing details
+print("Title: ",yt.title)
+print("Number of views: ",yt.views)
+print("Length of video: ",yt.length)
+print("Rating of video: ",yt.rating)
+#Getting the highest resolution possible
+ys = yt.streams.get_highest_resolution()
 
-          v.2
-
-Made by : Pheanoukmma
-
-    github : github.com/pheanoukma
-
-"""
-
-bruh = input("Input the path for video save :")
-
-SAVE_PATH = (bruh)
-
-link=open(' files.txt','r')
-
-for i in link:
-
-    try:
-
-        
-
-    YT = YouTube(i)
-
-    except:
-
-        print("connection error (check ur internet)")
-
-    mp4files = YT.filter('mp4')
-
-    d_video = YT.get(mp4files[-1].extension,mp4files[-1].resolution)
-
-    try : 
-
-        d_video.download(SAVE_PATH)
-
-    except : 
-
-        print("AN ERROR OCCURED, Try again later")
-
-print('Video downloaded!')   
+#Starting download
+print("Downloading..")
+ys.download()
+print("DL compleate")  
